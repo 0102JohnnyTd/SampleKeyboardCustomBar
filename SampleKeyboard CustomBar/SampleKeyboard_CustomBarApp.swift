30b179b2d86fce4c2def622c6093bf8596a2df01
0102JohnnyTd <rt2824.recruit.1022@gmail.com>
2024-11-04 09:59:55 +0900

    Initial Commit

---
+++ SampleKeyboard CustomBar/SampleKeyboard_CustomBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleKeyboard_CustomBarApp.swift
+//  SampleKeyboard CustomBar
+//
+//  Created by Johnny Toda on 2024/11/04.
+//
+
+import SwiftUI
+
+@main
+struct SampleKeyboard_CustomBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
